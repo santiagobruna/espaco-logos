a7af5c33f6fcbcee24c2a8dcc34ec10ab4a417dd
bruna <bruna@teste.com>
2025-06-05 15:44:21 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { GlobalStyle } from "./styles/GlobalStyle"
+
+function App() {
+  return (
+    <>
+      <GlobalStyle/>
+      <p className="text-3xl font-bold">
+        Espaço Logos
+      </p>
+    </>
+  )
+}
+
+export default App
---
+++ src/styles/GlobalStyle.ts
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from "styled-components";
+
+export const colors = {
+    colorMenu: '#4283AF',
+    colorButton: '#1D649B',
+    colorTitle: '#0069A6',
+    colorSection: '#F3DA46',
+    colorCard: '#EDE9E9',
+    colorFooter: '#C3DBF5'
+}
+
+export const GlobalStyle = createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        font-family: "Montserrat", sans-serif;
+    }
+`
